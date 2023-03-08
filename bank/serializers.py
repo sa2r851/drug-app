8cf68d85060f13f3508c4cf10c9376b73a92d491
Mohamed sakr <62855466+sa2r851@users.noreply.github.com>
2023-03-08 16:53:37 +0200

    Delete bank directory

--- bank/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-
-class TransactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields =fields =['sender','receiver','amount','transfer']
--- bank/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-class CartViewset(CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,GenericViewSet):
-    queryset=Cart.objects.all()
-    serializer_class=CartSerializer
