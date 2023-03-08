60cb5c606e4de57d44ca13acdee39899d734409a
Mohamed sakr <62855466+sa2r851@users.noreply.github.com>
2023-03-08 16:53:25 +0200

    Delete accounts directory

--- accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .serializers import MyTokenObtainPairSerializer
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-class MyObtainTokenPairView(TokenObtainPairView):
-    permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
